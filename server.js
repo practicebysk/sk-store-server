ff8afd6627c25f858dca08cb688fb2cd353eebfe
sk <kinarsardhara225@gamil.com>
2024-08-19 12:41:29 +0530

    change

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const app = require("./app");
 const cloudinary = require("cloudinary").v2;
+const cors = require('cors');
+
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Uncaught Exception`);
   process.exit(1);
 });
+
+
+app.use(cors({
+  origin: 'http://localhost:3000/', // Replace with your Netlify URL
+  methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
+  credentials: true // If you need to send cookies
+}));
+
 
 const server = app.listen(process.env.PORT, () => {
   console.log("Server is listening on http://localhost:" + process.env.PORT);
